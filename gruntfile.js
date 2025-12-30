2fd92eeaa3159cb31eb275e0f6e812061d41f404
marcusespino <marcusespino2001@gmail.com>
2025-12-30 14:46:15 -0300

    setup final

--- gruntfile.js
+++ gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
                         },
                         {
                             match:'ENDERECO_DO_JS',
-                            replacement: './src/script/main.min.js'
+                            replacement: './src/scripts/main.min.js'
                         }
                     ]
                 },
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
         uglify: {
             target: {
                 files: {
-                    'dist/script/main.min.js': 'src/script/main.js'
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
                 }
             }
         }
